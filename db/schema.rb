6dc0baffa3195483c3c9aa1f44e0687a0b4dc93f
Chad Jolly <cjolly@lawgical.com>
2018-02-24 13:19:01 -0700

    db schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20180224184623) do
-=======
 ActiveRecord::Schema.define(version: 20180224192624) do
->>>>>>> User
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,12 +60,6 @@ ActiveRecord::Schema.define(version: 20180224192624) do
     t.index ["document_id"], name: "index_reviews_on_document_id"
   end
 
-<<<<<<< HEAD
-  add_foreign_key "documents", "document_types"
-  add_foreign_key "ratings", "bullet_points"
-  add_foreign_key "ratings", "reviews"
-  add_foreign_key "reviews", "documents"
-=======
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "first_name"
@@ -82,5 +72,8 @@ ActiveRecord::Schema.define(version: 20180224192624) do
     t.datetime "updated_at", null: false
   end
 
->>>>>>> User
+  add_foreign_key "documents", "document_types"
+  add_foreign_key "ratings", "bullet_points"
+  add_foreign_key "ratings", "reviews"
+  add_foreign_key "reviews", "documents"
 end
