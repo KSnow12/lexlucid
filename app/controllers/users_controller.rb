f0b2da7721ea0c8eee5c244b07a29c8e4db9168a
Chad Jolly <cjolly@lawgical.com>
2018-02-24 18:27:17 -0700

    user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-  skip_before_action :require_login, except: [:index, :show, :destroy]
+  skip_before_action :require_login, except: [:index, :edit, :update, :destroy]
 
   # GET /users
   # GET /users.json
@@ -90,6 +90,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:email, :first_name, :last_name, :password, :website, :description, :company_name)
+      params.require(:user).permit(:email, :first_name, :last_name, :password, :website, :description, :company_name, :states, :attorney_registration_number)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   has_many :reviews
+  validates_uniqueness_of :email
   has_secure_password
 end
