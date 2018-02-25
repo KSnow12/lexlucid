a0462bc402fbb0bc76cd3d9842fa497a9e819ded
Chad Jolly <cjolly@lawgical.com>
2018-02-25 13:26:53 -0700

    some user links

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,13 @@ class User < ApplicationRecord
   def has_reviewed_document?(document)
     document.reviews.map(&:user_id).include?(self.id)
   end
+
+  def website
+    the_url = super
+    if the_url =~ %r{^https?://} || the_url.blank?
+      the_url
+    else
+      "http://#{the_url}"
+    end
+  end
 end
