5f83d075b4c57f5905da6d8e22a210e6f6ca7872
Nathan Benes <nathan.benes@gmail.com>
2018-02-25 18:00:22 -0700

    Precompile the svg logo

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,13 @@ module Lexlucid
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    config.assets.precompile += %w(
+      *.gif
+      *.png
+      *.jpg
+      *.svg
+    )
+
     # Don't generate system test files.
     config.generators.system_tests = nil
   end
