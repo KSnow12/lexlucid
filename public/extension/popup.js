e64a8067b4c81a6ed4542eada186836c2ab3886b
Carlos Gochez <cgotcha@gmail.com>
2018-03-01 12:17:49 -0700

    Chrome Extension: Grab the tabs information to clean the road to call the app and retrieve the data for the document

--- public/extension/popup.js
+++ public/extension/popup.js
@@ -1,4 +1,11 @@
 $(function () {
+
+  chrome.tabs.query({'active': true, 'currentWindow': true}, function (tabs) {
+    tab = tabs[0];
+    $('.js-document-title').html(tab.title);
+    $('.js-host-url').html(tab.url);
+  });
+
   $(".js-star-readonly-score").each(function() {
     return $(this).starRating({
       starSize: $(this).data('size') === 'small' ? 20 : 40,
