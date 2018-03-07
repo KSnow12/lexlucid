1cabcdb49946f5abb0353a29b2520287a3a86d78
Nathan Benes <nathan.benes@gmail.com>
2018-03-07 14:22:05 -0700

    Added active class to navbar elements if you are on specific pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def active_page?(*controller_actions)
+    controller_actions.each do |controller_action|
+      con_name, act_name = controller_action.split("#")
+
+      if con_name == controller_name && (!act_name || act_name == action_name)
+        return true
+      end
+    end
+    return false
+  end
 end
